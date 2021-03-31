c816c86701649cda8c6f6ff969f3b9e6e957bef9
Caspar <52498280+Caspar-Cheng@users.noreply.github.com>
2021-03-31 16:02:53 +1000

    Update Form.js

--- react-practice/src/components/Form.js
+++ react-practice/src/components/Form.js
@@ -1,36 +1,31 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import { useHistory } from 'react-router-dom';
-import { v4 as uuid } from 'uuid';
+import { useHistory } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 export const SubmitForm = () => {
-    const { addList } = useContext(GlobalContext);
-    const history = useHistory();
-    const [description, setDescription] = useState('');
-    const [category, setCategory] = useState('');
-    const [content, setContent] = useState('');
+  const { addList } = useContext(GlobalContext);
+  const history = useHistory();
+  const [form, setForm] = useState({
+    id: uuid(),
+    description: "",
+    category: "html",
+    content: "",
+  });
 
-    const onSubmit = () => {
-        const newForm = {
-            id: uuid(),
-            description: description,
-            category: category,
-            content: content
-        }
-        addList(newForm);
-        history.push("/todo");
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addList(form);
+    history.push("/todo");
+  };
 
-    const onDesChange = e => {
-        setDescription(e.target.value);  
-    };
-    const onCategoryChange = e => {
-        setCategory(e.target.value);
-    };
-    const onContentChange = e => {
-        setContent(e.target.value)
-    }
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  };
 
   return (
     <Form onSubmit={onSubmit}>
@@ -39,20 +34,20 @@ export const SubmitForm = () => {
           <Label for="description">Description: </Label>
           <Input
             type="text"
-            value={description}
-            onChange={onDesChange}
-            id="description"
-            style={{ maxWidth: "250px", marginLeft:"8px"}}
+            value={form.description}
+            onChange={onChange}
+            name="description"
+            style={{ maxWidth: 250, marginLeft: 8 }}
           ></Input>
         </div>
         <div className="d-flex my-2">
           <Label for="category">Category: </Label>
           <Input
             type="select"
-            value={category}
-            onChange={onCategoryChange}
-            id="category"
-            style={{ maxWidth: "100px", marginLeft:"8px"}}
+            value={form.category}
+            onChange={onChange}
+            name="category"
+            style={{ maxWidth: 100, marginLeft: 8 }}
           >
             <option>html</option>
             <option>css</option>
@@ -63,10 +58,10 @@ export const SubmitForm = () => {
           <Label for="content">Content: </Label>
           <Input
             type="textarea"
-            value={content}
-            onChange={onContentChange}
-            id="content"
-            style={{ maxWidth: "300px", marginLeft:"8px"}}
+            value={form.content}
+            onChange={onChange}
+            name="content"
+            style={{ maxWidth: 300, marginLeft: 8 }}
           ></Input>
         </div>
       </FormGroup>
