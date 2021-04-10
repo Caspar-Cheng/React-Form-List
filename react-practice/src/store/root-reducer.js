7813e34571ca4812f3e06f56dc097100c3b0aa84
Caspar <52498280+Caspar-Cheng@users.noreply.github.com>
2021-04-10 11:42:55 +1000

    Delete react-practice/src directory

--- react-practice/src/store/root-reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./user/reducers";
-
-export default combineReducers({
-  user: userReducer,
-});
--- react-practice/src/store/user/actions.js
+++
@@ -1,9 +0,0 @@
-export const deleteList = (id) => ({
-  type: "DELETE_LIST",
-  payload: id,
-});
-
-export const addList = (form) => ({
-  type: "ADD_LIST",
-  payload: form,
-});
